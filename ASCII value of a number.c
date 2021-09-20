170ab29c3cfa9c85f85ed1830bd2ebeaf1fe9995
Harikasaisabbella <91045499+Harikasaisabbella@users.noreply.github.com>
2021-09-20 18:29:31 +0530

    Add files via upload

---
+++ ASCII value of a number.c
@@ -0,0 +1,13 @@
+/* C program to find ASCII value of a number */
+#include<stdio.h>
+void main()
+{  // Declaring the variables
+   char character;
+   
+   // Taking inputs
+   printf("Enter a number: ");
+   scanf("%d", &character);
+   
+   // Printing the result
+   printf("Corresponding ASCII character for the given number is : %c", character);
+}
