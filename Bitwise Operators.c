71552875d54086834db9e2a64e3bffcba31ad952
Harikasaisabbella <91045499+Harikasaisabbella@users.noreply.github.com>
2021-09-21 11:37:37 +0530

    Update Bitwise Operators.c

--- Bitwise Operators.c
+++ Bitwise Operators.c
@@ -7,6 +7,8 @@ void main()
    
    // Taking inputs
    printf("value1\tvalue2\t&\t|\t^\n");
+   
+   // Printing the results
    printf("%d\t%d\t%d\t%d\t%d\n", value1, value2, value1&value2, value1|value2, value1^value2);
    printf("%d\t%d\t%d\t%d\t%d\n", 32, 55, 32&55, 32|55, 32^55);
    printf("%d\t%d\t%d\t%d\t%d\n", 74, 68, 74&68, 74|68, 74^68);
