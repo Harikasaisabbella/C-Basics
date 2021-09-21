585a334c3c51c0316d42193669c9b59d9fb92ac1
Harikasaisabbella <91045499+Harikasaisabbella@users.noreply.github.com>
2021-09-21 11:36:38 +0530

    Add files via upload

---
+++ Bitwise Operators.c
@@ -0,0 +1,16 @@
+/* Bitwise operators in C */
+#include<stdio.h>
+#include<math.h>
+void main()
+{  // Declaring the variables
+   int value1 = 98, value2 = 57;
+   
+   // Taking inputs
+   printf("value1\tvalue2\t&\t|\t^\n");
+   printf("%d\t%d\t%d\t%d\t%d\n", value1, value2, value1&value2, value1|value2, value1^value2);
+   printf("%d\t%d\t%d\t%d\t%d\n", 32, 55, 32&55, 32|55, 32^55);
+   printf("%d\t%d\t%d\t%d\t%d\n", 74, 68, 74&68, 74|68, 74^68);
+   printf("%d\t%d\t%d\t%d\t%d\n", 9, 30, 9&30, 9|30, 9^30);
+   
+   
+}
